b18e20cce230bcdeddfb674a3e8603e7b77f54be
Masao Maeda <brt.river@gmail.com>
2014-10-17 20:03:39 +0900

    Fix bug in interface

--- src/RequestInterface.php
+++ src/RequestInterface.php
@@ -3,15 +3,15 @@ namespace Haku;
 
 Interface RequestInterface
 {
-    public function query(string $key): mixed {}
+    public function query(string $key): mixed;
 
-    public function request(string $key): mixed {}
+    public function request(string $key): mixed;
 
-    public function server(string $key): mixed {}
+    public function server(string $key): mixed;
     
-    public function queryAll(): Map<string, mixed> {}
+    public function queryAll(): Map<string, mixed>;
 
-    public function requestAll(): Map<string, mixed> {}
+    public function requestAll(): Map<string, mixed>;
 
-    public function serverAll(): Map<string, mixed> {}
+    public function serverAll(): Map<string, mixed>;
 }
